e10480c842b83540d450251593bc90d4df6a518d
James Hoctor <JEHoctor@protonmail.com>
2026-02-08 16:06:39 -0500

    Run app in dev mode within podman (except when running from PyPI) and connect to host dev console

--- podman/test-chat/entrypoint-editable.sh
+++ podman/test-chat/entrypoint-editable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-/home/ubuntu/.local/bin/uv run chat "$@" || true
+/home/ubuntu/.local/bin/uv run textual run --dev --host=host.containers.internal -c chat "$@" || true
 
 exec /bin/bash
